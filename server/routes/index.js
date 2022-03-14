a9ecd0e5253bfba73935bf884e55c1f8334edfec
khaled-samy <khaled.s.elkhudary@gmail.com>
2022-03-14 16:53:36 +0200

    handle log in page

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,11 +5,13 @@ const {
   signUp,
   logIn,
   handleSignUp,
+  loginHandle,
 } = require('../controllers');
 
 router.get('/posts', getAllPosts);
 router.get('/sign-up', handleSignUp);
 router.post('/sign-up', signUp);
+router.get('/log-in', loginHandle);
 router.post('/log-in', logIn);
 
 module.exports = router;
