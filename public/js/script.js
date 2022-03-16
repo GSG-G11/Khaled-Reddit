be9794469c387231e62dbd5e32cda2dbe72dd29c
khaled-samy <khaled.s.elkhudary@gmail.com>
2022-03-16 14:43:56 +0200

    handle user home auth

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,24 @@
+import getUserName from './getUserInfFromCookie.js';
+import { renderDom, renderDomUser, clearPosts } from './dom.js';
+
+const name = document.querySelector('#name');
+
+const userInfo = getUserName();
+name.textContent = userInfo.username;
+
 fetch('/posts')
   .then((res) => res.json())
   .then((data) => {
     data.forEach((object) => {
       renderDom(object);
+    });
+  });
+
+fetch('/user/posts')
+  .then((res) => res.json())
+  .then((data) => {
+    clearPosts();
+    data.forEach((object) => {
+      renderDomUser(object);
     });
   });
